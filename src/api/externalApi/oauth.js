70ad29d868dfa6d21d090f0125dfe792c403282c
Emma RULLIERE <emma.rulliere@epitech.eu>
2022-06-20 13:44:29 +0200

    [FIX] last merge error

--- src/api/externalApi/oauth.js
+++ src/api/externalApi/oauth.js
@@ -1,12 +1,7 @@
 const { Router } = require('express');
 const router = new Router();
-<<<<<<< HEAD
 const facebook = require('./facebook');
 const google = require('./google');
-=======
-const google = require('./facebook');
-const discord = require('./google');
->>>>>>> c9fcd16f4aa5189149f83dbb4c6fa4066e6e299f
 
 router.use('/google', google);
 router.use('/facebook', facebook);
