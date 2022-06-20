77a035f6497c2b22bb6a8afaaad75ad231a4b4d1
Magiikarpe <75068193+Magiikarpe@users.noreply.github.com>
2022-06-20 13:43:22 +0200

    Dev (#5)
    * 3.5.1 dev serveur (#3)
    
    * Create azure-webapps-node.yml
    
    * Create unit_test_launcher.yml
    
    * Create unit_test
    
    * Update unit_test_launcher.yml
    
    * [ADD] update preprod for test (#1)
    
    * Create azure-webapps-node.yml
    
    * Create unit_test_launcher.yml
    
    * Create unit_test
    
    * Update unit_test_launcher.yml
    
    Co-authored-by: victor-thouvenin <71706244+victor-thouvenin@users.noreply.github.com>
    
    * Create sum.test.js
    
    * [ADD] 3.4.1 architecture (#2)
    
    * [ADD] base server and docker-compose working
    
    * [DEL] readme.md
    
    * [ADD] beautifull readme template
    
    * [MOD] change title of readme page
    
    * [ADD] architecture sprint1
    
    * [MOD] implementation routes architecture
    
    * [MOD] implementation of the architecture for connection and register functions
    
    * Create sum.js
    
    * Create sum.js
    
    * Create app.js
    
    * [ADD] test package
    
    * [ADD] ouath facebook, google and normal connexion.
    
    * [MOD] FIX SERVER
    
    * [MOD] trying to fix register route
    
    Co-authored-by: victor-thouvenin <71706244+victor-thouvenin@users.noreply.github.com>
    
    * [FIX] route
    
    * [DEL] app folder
    
    * [FIX] init_db
    
    * [FIX] merge
    
    * [DEL] app
    
    Co-authored-by: victor-thouvenin <71706244+victor-thouvenin@users.noreply.github.com>

--- src/api/externalApi/google.js
+++ src/api/externalApi/google.js
@@ -1,6 +1,127 @@
 const { Router } = require('express');
 const router = new Router();
+<<<<<<< HEAD
+const { google_clientID, google_secret } = require('../../const.json');
+const axios = require('axios');
+const querystring = require('querystring');
+const USER = require('../../persistence/users')
+const TOKEN = require('../../persistence/tokens')
+const jwt = require('jsonwebtoken');
+const { jwt_key } = require('../../const.json');
+
+function getGoogleAuthURL(redirect) {
+    const rootUrl = "https://accounts.google.com/o/oauth2/v2/auth";
+    const options = {
+        redirect_uri: redirect,
+        client_id: `${google_clientID}`,
+        access_type: "offline",
+        response_type: "code",
+        prompt: "consent",
+        scope: [
+        'https://www.googleapis.com/auth/calendar',
+        'https://mail.google.com/',
+        'https://www.googleapis.com/auth/drive',
+        'https://www.googleapis.com/auth/userinfo.profile',
+        'https://www.googleapis.com/auth/userinfo.email'
+        ].join(" ")
+    };
+    return `${rootUrl}?${querystring.stringify(options)}`;
+}
+
+router.get("/url", (request, response) => {
+    return response.send(getGoogleAuthURL('http://localhost:8000/oauth/google'));
+});
+
+router.get("/urlLogin", (request, response) => {
+    return response.send(getGoogleAuthURL('http://localhost:8000/login/google'));
+});
+
+async function getTokens(code) {
+    const url = "https://oauth2.googleapis.com/token";
+    const values = {
+        code,
+        client_id: google_clientID,
+        client_secret: google_secret,
+        redirect_uri: 'http://localhost:8000/oauth/google',
+        grant_type: "authorization_code",
+    };
+    tokens = await axios
+        .post(url, querystring.stringify(values), {
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        })
+    return tokens.data
+    }
+
+    router.get("/", async (req, response) => {
+    try {
+        const { access_token } = await getTokens(req.query.code)
+        return response.json(access_token)
+    } catch (e) {
+        console.error(e)
+    }
+})
+
+async function getLoginTokens(code) {
+    const url = "https://oauth2.googleapis.com/token";
+    const values = {
+        code,
+        client_id: google_clientID,
+        client_secret: google_secret,
+        redirect_uri: 'http://localhost:8000/login/google',
+        grant_type: "authorization_code",
+    };
+    tokens = await axios
+        .post(url, querystring.stringify(values), {
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        })
+    return tokens.data
+}
+
+router.get("/login", async (req, response) => {
+    try {
+
+        const { id_token, access_token } = await getLoginTokens(req.query.code)
+        const user = await axios
+        .get(
+            `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${access_token}`,
+            {
+            headers: {
+                Authorization: `Bearer ${id_token}`,
+            },
+            }
+        )
+        const checkUser = await USER.find(user.data.email)
+        if (checkUser) {
+        TOKEN.set(checkUser.email, 'google', access_token, (status) => {
+            // pass
+        })
+        return response.status(200).json({
+            message: "Connected with google",
+            jwt: jwt.sign({user: {id: checkUser.id, email: checkUser.email }}, jwt_key)
+        })
+        } else {
+        USER.create(user.data.email, '').then(user => {
+            TOKEN.set(user.email, 'google', access_token, (status) => {
+            // pass
+            })
+            const jwtToken = jwt.sign({ user }, jwt_key);
+            return response.status(200).json({
+            message: "Connected with google",
+            jwt: jwtToken
+            })
+        })
+        }
+    } catch (e) {
+        console.error(e)
+    }
+})
+=======
 
 //implementer fonctions oauth google
+>>>>>>> c9fcd16f4aa5189149f83dbb4c6fa4066e6e299f
 
 module.exports = router;
