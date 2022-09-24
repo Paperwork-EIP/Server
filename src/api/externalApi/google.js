6262ed0790551277ec7050233e5816006b944d3c
Magiikarpe <75068193+Magiikarpe@users.noreply.github.com>
2022-09-24 15:53:02 +0900

    Cookies and tokens er (#19)
    * [ADD] cookies and tokens functionnal but error display when google and facebook oauth? working but bad request?
    
    * [FIX] cookies and tokens gestion working

--- src/api/externalApi/google.js
+++ src/api/externalApi/google.js
@@ -2,78 +2,59 @@ const { Router } = require('express');
 const router = new Router();
 const { google_clientID, google_secret } = require('../../const.json');
 const axios = require('axios');
-const querystring = require('querystring');
-const USER = require('../../persistence/users')
-const TOKEN = require('../../persistence/tokens')
+const USER = require('../../persistence/users');
+const TOKEN = require('../../persistence/tokens');
 const jwt = require('jsonwebtoken');
 const { jwt_key } = require('../../const.json');
-const REDIRECT_URI = 'http://localhost:3000/home';
+const REDIRECT_URI = 'http://localhost:3000/googleLogin';
+const {URLSearchParams} = require('url');
 
-function getGoogleAuthURL(redirect) {
+function getGoogleAuthURL() {
     const rootUrl = "https://accounts.google.com/o/oauth2/v2/auth";
-    const options = {
-        redirect_uri: redirect,
-        client_id: `${google_clientID}`,
-        access_type: "offline",
-        response_type: "code",
-        prompt: "consent",
-        scope: [
-        'https://www.googleapis.com/auth/calendar',
-        'https://mail.google.com/',
-        'https://www.googleapis.com/auth/drive',
-        'https://www.googleapis.com/auth/userinfo.profile',
-        'https://www.googleapis.com/auth/userinfo.email'
-        ].join(" ")
-    };
-    return `${rootUrl}?${querystring.stringify(options)}`;
+    return `${rootUrl}?${new URLSearchParams([
+        ['client_id', google_clientID],
+        ['access_type', "offline"],
+        ['redirect_uri', REDIRECT_URI],
+        ['response_type', "code"],
+        ['prompt', "consent"],
+        ['scope', [
+            'https://www.googleapis.com/auth/calendar',
+            'https://mail.google.com/',
+            'https://www.googleapis.com/auth/drive',
+            'https://www.googleapis.com/auth/userinfo.profile',
+            'https://www.googleapis.com/auth/userinfo.email'
+            ].join(" ")]
+      ]).toString()}`;
 }
 
 router.get("/url", (request, response) => {
-    return response.send(getGoogleAuthURL(REDIRECT_URI));
+    return response.send(getGoogleAuthURL());
 });
 
 router.get("/urlLogin", (request, response) => {
-    return response.send(getGoogleAuthURL(REDIRECT_URI));
+    return response.send(getGoogleAuthURL());
 });
 
-async function getTokens(code) {
-    const url = "https://oauth2.googleapis.com/token";
-    const values = {
-        code,
-        client_id: google_clientID,
-        client_secret: google_secret,
-        redirect_uri: REDIRECT_URI,
-        grant_type: "authorization_code",
-    };
-    tokens = await axios
-        .post(url, querystring.stringify(values), {
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-        },
-        })
-    return tokens.data
-    }
-
-    router.get("/", async (req, response) => {
-    try {
-        const { access_token } = await getTokens(req.query.code)
-        return response.json(access_token)
-    } catch (e) {
-        console.error(e)
-    }
-})
+// router.get("/", async (req, response) => {
+//     try {
+//         const { access_token } = await getLoginTokens(req.query.code)
+//         return response.json(access_token)
+//     } catch (e) {
+//         console.error(e)
+//     }
+// });
 
 async function getLoginTokens(code) {
     const url = "https://oauth2.googleapis.com/token";
-    const values = {
-        code,
-        client_id: google_clientID,
-        client_secret: google_secret,
-        redirect_uri: REDIRECT_URI,
-        grant_type: "authorization_code",
-    };
+    const values = new URLSearchParams([
+        ['client_id', google_clientID],
+        ['code', code],
+        ['redirect_uri', REDIRECT_URI],
+        ['client_secret', google_secret],
+        ['grant_type', "authorization_code"]
+    ]).toString();
     tokens = await axios
-        .post(url, querystring.stringify(values), {
+        .post(url, values, {
         headers: {
             "Content-Type": "application/x-www-form-urlencoded",
         },
@@ -83,7 +64,6 @@ async function getLoginTokens(code) {
 
 router.get("/login", async (req, response) => {
     try {
-
         const { id_token, access_token } = await getLoginTokens(req.query.code)
         const user = await axios
         .get(
@@ -93,27 +73,23 @@ router.get("/login", async (req, response) => {
                 Authorization: `Bearer ${id_token}`,
             },
             }
-        )
-        const checkUser = await USER.find(user.data.email)
+            )
+        const checkUser = await USER.find(user.data.email);
         if (checkUser) {
-        TOKEN.set(checkUser.email, 'google', access_token, (status) => {
-            // pass
-        })
-        return response.status(200).json({
-            message: "Connected with google",
-            jwt: jwt.sign({user: {id: checkUser.id, email: checkUser.email }}, jwt_key)
-        })
-        } else {
-        USER.create(user.data.email, '').then(user => {
-            TOKEN.set(user.email, 'google', access_token, (status) => {
-            // pass
-            })
-            const jwtToken = jwt.sign({ user }, jwt_key);
+            TOKEN.set(checkUser.email, 'google', access_token);
             return response.status(200).json({
-            message: "Connected with google",
-            jwt: jwtToken
+                message: "Connected with google",
+                jwt: jwt.sign({user: {id: checkUser.id, email: checkUser.email }}, jwt_key)
             })
-        })
+        } else {
+            USER.create(user.data.id, user.data.email, access_token).then(user => {
+                TOKEN.set(user.email, 'google', access_token);
+                const jwtToken = jwt.sign({ user }, jwt_key);
+                return response.status(200).json({
+                message: "Connected with google",
+                jwt: jwtToken
+                })
+            })
         }
     } catch (e) {
         console.error(e)
