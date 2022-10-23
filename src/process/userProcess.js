e1dfc40a470ce98463986d7da0f7dffa06b879dc
Magiikarpe <75068193+Magiikarpe@users.noreply.github.com>
2022-10-24 03:33:22 +0900

    [ADD] User process and user step create and get user steps working (#26)
    * [ADD] create user process and steps based on the user responce working
    
    * [MOD] data base fix

---
+++ src/process/userProcess.js
@@ -0,0 +1,52 @@
+const { Router } = require('express');
+const Step = require('../persistence/step');
+const UserProcess = require('../persistence/userProcess');
+const UserStep = require('../persistence/userSteps');
+const router = new Router();
+  
+  router.post('/add', async (request, response) => {
+    try {
+        const { process_title, user_email, questions } = request.body;
+        const res = await UserProcess.create(process_title, user_email);
+        for (var i in questions) {
+          await UserStep.create(questions[i][0], questions[i][1], res.id);
+        }
+        return response.status(200).json({
+            message: 'User process and user steps created!',
+            response: res
+        });
+    } catch (error) {
+        return response.status(500).json({ message: 'System error.' });
+    }
+  });
+  router.get('/delete', async (request, response) => {
+    try {
+        const { title } = request.body;
+        const res = await UserProcess.delete(title);
+        return response.status(200).json({
+            message: 'UserProcess delete!',
+            response: res 
+        });
+    } catch (error) {
+        return response.status(500).json({ message: 'System error.' });
+    }
+  });
+  router.get('/getUserSteps', async (request, response) => {
+    try {
+      const { process_title, user_email } = request.body;
+      const userStep = await UserProcess.getUserSteps(process_title, user_email);
+      var task = [];
+      var step = null;
+      for (var i in userStep) {
+        if (userStep[i].is_done == false) {
+          step = await Step.getById(userStep[i].step_id);
+          task.push([step.title, step.type, step.description, step.source]);
+        }
+      }
+      return response.status(200).json({task});
+    } catch (error) {
+      return response.status(500).json({ message: 'System error.' });
+    }
+  });
+
+module.exports = router;
