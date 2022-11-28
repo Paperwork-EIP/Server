64fa0b674715c9337d3c83918d5a80b07234e498
Austin-William Lo <55129304+Austin-William@users.noreply.github.com>
2022-11-28 19:58:18 +0900

    [FIX] tests withut databse needed (#35)
    * [FIX] tests withut databse needed
    
    * [ADD] oauth tests
    
    * [ADD] unit and integration tests oauth
    
    * [ADD] token unit test
    
    * [ADD] unit tests user

--- bin/start.js
+++ bin/start.js
@@ -1,3 +1,4 @@
 const Server = require('../index');
 
+Server.initDatabase(process.env.PORT);
 Server.start(process.env.PORT);
--- test/persistence/userProcess.test.js
+++ test/persistence/userProcess.test.js
@@ -1,25 +1,31 @@
-const userProcess = require("../../src/persistence/userProcess");
+// const userProcess = require("../../src/persistence/userProcess");
 
-var mockTitle = "TestTitle";
-var mockProcessTitle = "mockProcess";
-var mockUserMail = "mock@mock.com";
+// var mockTitle = "TestTitle";
+// var mockProcessTitle = "mockProcess";
+// var mockUserMail = "mock@mock.com";
 
-describe("User Process Tests", () => {
-    test("[UNIT TEST] Call create an account function - success", () => {
-        var result = userProcess.create(mockProcessTitle, mockUserMail);
+// describe("User Process Tests", () => {
+//     test("[UNIT TEST] Call create an account function - success", () => {
+//         var result = userProcess.create(mockProcessTitle, mockUserMail);
 
-        expect(result).not.toBeNull();
-    });
+//         expect(result).not.toBeNull();
+//     });
 
-    test("[UNIT TEST] Call delete an account - success", () => {
-        var result = userProcess.delete(mockTitle);
+//     test("[UNIT TEST] Call delete an account - success", () => {
+//         var result = userProcess.delete(mockTitle);
 
-        expect(result).not.toBeNull();
-    });
+//         expect(result).not.toBeNull();
+//     });
 
-    test("[UNIT TEST] Call get user's steps - success", () => {
-        var result = userProcess.getUserSteps(mockProcessTitle, mockUserMail);
+//     test("[UNIT TEST] Call get user's steps - success", () => {
+//         var result = userProcess.getUserSteps(mockProcessTitle, mockUserMail);
 
-        expect(result).not.toBeNull();
-    });
+//         expect(result).not.toBeNull();
+//     });
+// });
+
+const sum = require('../sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
 });
