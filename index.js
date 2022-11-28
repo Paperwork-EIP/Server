0cbc417dea97faf07caf396f3de4159440ca21c6
Austin-William Lo <55129304+Austin-William@users.noreply.github.com>
2022-11-28 19:58:18 +0900

    [FIX] tests withut databse needed (#35)
    * [FIX] tests withut databse needed
    
    * [ADD] oauth tests
    
    * [ADD] unit and integration tests oauth
    
    * [ADD] token unit test
    
    * [ADD] unit tests user

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
+const helmet = require("helmet");
 const bodyParser = require('body-parser');
 const route = require('./src/api/index');
 const process = require('./src/process/index');
 
+const appSecure = express();
+appSecure.disable("x-powered-by");
+
+const app = express();
+app.use(helmet.hidePoweredBy());
 app.use(cors());
 app.use(bodyParser.json());
 
-const init_db = require('./src/persistence/init-db')
-init_db.initAll()
+function initDatabase(port) {
+  if (port === 3001) {
+    return;
+  }
+  else {
+    const init_db = require('./src/persistence/init-db');
+    init_db.initAll();
+  }
+}
 
 let server;
-module.exports = {
-  start(port) {
-    app.use(route, process);
-    server = app.listen(port, () => {
-      console.log(`App started on port ${port}`);
-    });
-    return app;
-  },
-  stop() {
-    server.close();
-  }
-};
+
+function start(port) {
+  app.use(route, process);
+  server = app.listen(port, () => {
+    console.log(`App started on port ${port}`);
+  });
+  return server;
+}
+function stop() {
+  server.close();
+}
+
+module.exports = { initDatabase, start, stop };
---
+++ test/api/oauth.test.js
@@ -0,0 +1,163 @@
+const request = require("supertest");
+const router = require("../../src/api/externalApi/oauth");
+const { start, stop } = require('../../index');
+
+describe("OAuth connections tests", () => {
+    const port = 3002;
+    let server;
+
+    beforeAll(() => {
+        server = start(port);
+    });
+
+    afterAll(() => {
+        stop();
+    });
+
+    describe("[UNIT TESTS]", () => {
+        describe("[VALID OAUTH TESTS]", () => {
+            test("should have a router component", () => {
+                expect(router).not.toBeNull();
+            });
+            test("should have instanced the router component", () => {
+                expect(router).toBeDefined();
+            });
+            describe("--- OAUTH GOOGLE ---", () => {
+                test("'/' should exist", async () => {
+                    const payload = {};
+                    const response = await request(server).get('/oauth/google/urlLogin').send(payload);
+
+                    expect(response.notFound).toEqual(false);
+                });
+            });
+
+            describe("--- OAUTH FACEBOOK ---", () => {
+                test("'/' should exist", async () => {
+                    const payload = {};
+                    const response = await request(server).get('/oauth/facebook/url').send(payload);
+
+                    expect(response.notFound).toEqual(false);
+                });
+            });
+        });
+    });
+
+    describe("[INTEGRATION TESTS]", () => {
+        describe("[VALID OAUTH TESTS]", () => {
+            describe("--- OAUTH GOOGLE ---", () => {
+                // test("should receive a valid response for route '/'", async () => {
+                //     const payload = {
+                //         query: {
+                //             code: "dkgfbfddbfb"
+                //         }
+                //     };
+                //     const response = await request(server).get("/oauth/google/").send(payload);
+
+                //     expect(response).not.toBeNull()
+                //     expect(response.status).toEqual(200);
+                //     expect(response.req.header).not.toBeNull();
+                //     expect(response.text).not.toBeNull();
+                //     expect(response.header).not.toBeNull();
+                //     expect(response.headers).not.toBeNull();
+                //     expect(response.header['content-type']).toEqual("text/html; charset=utf-8");
+                //     expect(response.headers['content-type']).toEqual("text/html; charset=utf-8");
+                // });
+                test("should receive a valid response for route '/urlLogin'", async () => {
+                    const payload = {};
+                    const response = await request(server).get("/oauth/google/urlLogin").send(payload);
+
+                    expect(response).not.toBeNull()
+                    expect(response.status).toEqual(200);
+                    expect(response.req.header).not.toBeNull();
+                    expect(response.text).not.toBeNull();
+                    expect(response.header).not.toBeNull();
+                    expect(response.headers).not.toBeNull();
+                    expect(response.header['content-type']).toEqual("text/html; charset=utf-8");
+                    expect(response.headers['content-type']).toEqual("text/html; charset=utf-8");
+                });
+            });
+
+            describe("--- OAUTH FACEBOOK ---", () => {
+                // test("should receive a valid response for route '/'", async () => {
+                //     const payload = {
+                //         query: {
+                //             code: "dkgfbfddbfb"
+                //         }
+                //     };
+                //     const response = await request(server).get("/oauth/google/").send(payload);
+
+                //     expect(response).not.toBeNull()
+                //     expect(response.status).toEqual(200);
+                //     expect(response.req.header).not.toBeNull();
+                //     expect(response.text).not.toBeNull();
+                //     expect(response.header).not.toBeNull();
+                //     expect(response.headers).not.toBeNull();
+                //     expect(response.header['content-type']).toEqual("text/html; charset=utf-8");
+                //     expect(response.headers['content-type']).toEqual("text/html; charset=utf-8");
+                // });
+                test("should receive a valid response for route '/url'", async () => {
+                    const payload = {};
+                    const response = await request(server).get("/oauth/facebook/url").send(payload);
+
+                    expect(response).not.toBeNull()
+                    expect(response.status).toEqual(200);
+                    expect(response.req.header).not.toBeNull();
+                    expect(response.text).not.toBeNull();
+                    expect(response.header).not.toBeNull();
+                    expect(response.headers).not.toBeNull();
+                    expect(response.header['content-type']).toEqual("text/html; charset=utf-8");
+                    expect(response.headers['content-type']).toEqual("text/html; charset=utf-8");
+                });
+            });
+        });
+
+        describe("[INVALID OAUTH TESTS", () => {
+            describe("--- LOGIN ---", () => {
+                // test("should return an error for route '/' with an 400 status code", async () => {
+                //     const response = await request(server).get("/oauth/google/").send({});
+
+                //     expect(response.status).toBe(400);
+                // });
+                // test("id_token missing : should receive an error 400 status code", async () => {
+                //     const response = await request(server).get("/oauth/google/login").send({
+                //         access_token: "fdsfdsgfbfdssbdfdfvdfvd"
+                //     });
+
+                //     expect(response.status).toBe(400);
+                // });
+                // test("id_token empty : should receive an error 400 status code", async () => {
+                //     const response = await request(server).get("/oauth/google/login").send({
+                //         id_token: "",
+                //         access_token: "fdsfdsgfbfdssbdfdfvdfvd"
+                //     });
+
+                //     expect(response.status).toBe(400);
+                // });
+                // test("access_token missing : should receive an error 400 status code", async () => {
+                //     const payload = {
+                //         id_token: "fdsfdsgfbfdssbdfdfvdfvd"
+                //     };
+                //     const response = await request(server).get("/oauth/google/login").send(payload);
+
+                //     expect(response.status).toBe(400);
+                //     expect(response).toBeCalledWith(payload);
+                // });
+                // test("access_token empty : should receive an error 400 status code", async () => {
+                //     const response = await request(server).get("/oauth/google/login").send({
+                //         id_token: "fdsfdsgfbfdssbdfdfvdfvd",
+                //         access_token: ""
+                //     });
+
+                //     expect(response.status).toBe(400);
+                // });
+            });
+            describe("--- OAUTH GOOGLE ---", () => {
+
+            });
+
+            describe("--- OAUTH FACEBOOK ---", () => {
+
+            });
+        });
+    });
+});
