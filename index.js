45173e9b3205ef69a941a76200dc8aa89b0e0bb2
Austin-William Lo <55129304+Austin-William@users.noreply.github.com>
2022-12-23 02:04:50 +0900

    [ADD] database test with Docker

--- index.js
+++ index.js
@@ -1,43 +1,26 @@
 const express = require('express');
 const cors = require('cors');
-const helmet = require("helmet");
+const app = express();
 const bodyParser = require('body-parser');
 const route = require('./src/api/index');
 const process = require('./src/process/index');
 
-const appSecure = express();
-appSecure.disable("x-powered-by");
-
-let corsOptions = {
-  origin: 'http://localhost:8080/'
-};
-
-const app = express();
-app.use(helmet.hidePoweredBy());
+app.use(cors());
 app.use(bodyParser.json());
-app.use(cors(corsOptions));
 
-function initDatabase(port) {
-  if (port === 3001) {
-    return;
-  }
-  else {
-    const init_db = require('./src/persistence/init-db');
-    init_db.initAll();
-  }
-}
+const init_db = require('./src/persistence/init-db')
+init_db.initAll()
 
 let server;
-
-function start(port) {
-  app.use(route, process);
-  server = app.listen(port, () => {
-    console.log(`App started on port ${port}`);
-  });
-  return server;
-}
-function stop() {
-  server.close();
-}
-
-module.exports = { initDatabase, start, stop };
+module.exports = {
+  start(port) {
+    app.use(route, process);
+    server = app.listen(port, () => {
+      console.log(`App started on port ${port}`);
+    });
+    return app;
+  },
+  stop() {
+    server.close();
+  }
+};
