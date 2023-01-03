50a6024cbd47be826ddca1f3815c213fa8125fb0
Magiikarpe <75068193+Magiikarpe@users.noreply.github.com>
2023-01-03 22:29:47 +0900

    Preprod (#74)
    * [ADD] local service for dev (#69)
    
    * Fix tests (#68)
    
    * add docker-compose for prod
    
    * [FIX] tests
    
    * [ADD] async
    
    Co-authored-by: annaig leizour <annaig.leizour@epitech.eu>
    
    * Process idea (#73)
    
    * [ADD] process proposal system, [MOD] doc api, [FIX] docker-compose serverLocal
    
    * [MOD] user register email returned
    
    Co-authored-by: Austin-William Lo <55129304+Austin-William@users.noreply.github.com>
    Co-authored-by: annaig leizour <annaig.leizour@epitech.eu>

---
+++ src/persistence/processProposal.js
@@ -0,0 +1,52 @@
+const db = require('./db');
+const sql = require('sql-template-strings');
+
+module.exports = {
+    async create(title, description, content, user_id) {
+        try {
+            const current_date = new Date();
+            const { rows } = await db.query(sql`
+            INSERT INTO process_proposal (title, description, content, date, user_id, is_in_process)
+                VALUES (${title}, ${description}, ${content}, ${current_date}, ${user_id}, false)
+                RETURNING id, title;
+            `);
+            const [process_proposal] = rows;
+            return process_proposal;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    },
+    async getAll() {
+        try {
+            const { rows } = await db.query(sql`
+            SELECT * FROM process_proposal
+            `);
+            return rows;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    },
+    async get(id) {
+        try {
+            const { rows } = await db.query(sql`
+            SELECT * FROM process_proposal WHERE id=${id} LIMIT 1;
+            `);
+            return rows[0];
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    },
+    async delete(id) {
+        try {
+            const { rows } = await db.query(sql`
+            DELETE FROM process_proposal where id=${id};`);
+            return rows[0];
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    },
+}
--- src/process/index.js
+++ src/process/index.js
@@ -6,11 +6,13 @@ const step = require('./step.js');
 const questions = require('./questions.js');
 const userProcess = require('./userProcess.js');
 const calendar = require('./calendar.js');
+const proseccProposal = require('./processProposal.js');
 
 router.use('/process', process);
 router.use('/step', step);
 router.use('/processQuestions', questions);
 router.use('/userProcess', userProcess);
 router.use('/calendar', calendar);
+router.use('/processProposal', proseccProposal);
 
 module.exports = router;
