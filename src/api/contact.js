0da4f3e35def3ad118cc7e0cad46c97d3991e986
Emma RULLIERE <emma.rulliere@epitech.eu>
2023-05-20 04:12:33 +0900

    [FIX] code smells

--- src/api/contact.js
+++ src/api/contact.js
@@ -18,7 +18,7 @@ router.get('/sendEmail', async (request, response) => {
                 Body: {
                     Html: {
                         Charset: 'UTF-8',
-                        Data: `<!DOCTYPE html><html lang="en\"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>${name}</title></head><body><h1>Process proposition send by ${email}</h1><p>${description}</p></body></html>`
+                        Data: `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>${name}</title></head><body><h1>Process proposition send by ${email}</h1><p>${description}</p></body></html>`
                     }
                 },
                 Subject: {
--- src/api/db/user.js
+++ src/api/db/user.js
@@ -236,7 +236,7 @@ router.post('/register', async (request, response) => {
             Body: {
               Html: {
                 Charset: 'UTF-8',
-                Data: `<!DOCTYPE html><html lang="en\"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Verification Email</title></head><body><h1>Verify your email</h1><p>Click on the following link to verify your email:</p><a href="${process.env.URL}/verifyEmail?token=${token}">${process.env.URL}/verifyEmail?token=${token}</a></body></html>`
+                Data: `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Verification Email</title></head><body><h1>Verify your email</h1><p>Click on the following link to verify your email:</p><a href="${process.env.URL}/verifyEmail?token=${token}">${process.env.URL}/verifyEmail?token=${token}</a></body></html>`
               }
             },
             Subject: {
